f4958233fb24f693cf0b96f803dc72fb46d415c8
Chris Zubak-Skees <chriszs@gmail.com>
2019-06-01 00:25:41 -0400

    Update pipelines

--- transforms/download.sh
+++ transforms/download.sh
@@ -7,6 +7,7 @@ ID=zoIeC19F3q8
 EXT=mp4
 
 if [ ! -f $1"/"$ID"."$EXT ]; then
+	# streamlink --stdout https://www.youtube.com/watch?v=sHT1bYAlCIk best | ffmpeg -i pipe:0 -c copy -f segment -segment_time 10 -reset_timestamps 1 -map 0 ./%d".mp4"
 	pipenv run youtube-dl -f "bestvideo[ext="$EXT"]" -o $1"%(id)s.%(ext)s" --write-info-json "https://youtu.be/"$ID
 fi
 #done
